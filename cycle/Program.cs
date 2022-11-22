9609b8b1af169ab061dbf82a1cbb1e494c579f26
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-23 00:54:30 +0500

    added info about cycle

---
+++ cycle/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+double A = ReadInt("Введите число А ");
+double B = ReadInt("Введите число B ");
+double degree = Math.Pow(A, B);
+while( B > 0)
+{
+Console.WriteLine($"{A}, {B} -> {degree}");
+return;
+}
+
+double ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToDouble(Console.ReadLine());
+}
